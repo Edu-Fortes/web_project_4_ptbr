3ad7cab1ca5f6dcf5e6fc5ce78993fffc7ad13fd
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-03-02 06:13:53 -0300

    hotfix(script): replace variable let per const
    Since let is used for variable that change its value, replaced per const variable

--- script/index.js
+++ script/index.js
@@ -1,19 +1,19 @@
-let modal = document.querySelector(".popup");
-let closedModal = modal.classList.remove("popup_opened");
+const modal = document.querySelector(".popup");
+const closedModal = modal.classList.remove("popup_opened");
 
 // ABRE E FECHA O MODAL
 
-let openModalBtn = document.querySelector(".button_edit");
-let closeModalBtn = modal.querySelector(".button_close");
+const openModalBtn = document.querySelector(".button_edit");
+const closeModalBtn = modal.querySelector(".button_close");
 
-let openCloseModal = function () {
+const openCloseModal = function () {
   modal.classList.toggle("popup_opened");
 
-  let nameInput = modal.querySelector(".popup__input_type_name");
-  let aboutInput = modal.querySelector(".popup__input_type_about");
+  const nameInput = modal.querySelector(".popup__input_type_name");
+  const aboutInput = modal.querySelector(".popup__input_type_about");
 
-  let perfilName = document.querySelector(".profile__title");
-  let perfilAbout = document.querySelector(".profile__subtitle");
+  const perfilName = document.querySelector(".profile__title");
+  const perfilAbout = document.querySelector(".profile__subtitle");
 
   nameInput.value = perfilName.textContent;
   aboutInput.value = perfilAbout.textContent;
@@ -24,17 +24,17 @@ closeModalBtn.addEventListener("click", openCloseModal);
 
 // ENVIA FORMULARIO
 
-let formElement = modal.querySelector(".popup__form");
-let saveBtn = modal.querySelector(".button_save");
+const formElement = modal.querySelector(".popup__form");
+const saveBtn = modal.querySelector(".button_save");
 
 function handleProfileFormSubmit(e) {
   e.preventDefault();
 
-  let nameInput = modal.querySelector(".popup__input_type_name");
-  let aboutInput = modal.querySelector(".popup__input_type_about");
+  const nameInput = modal.querySelector(".popup__input_type_name");
+  const aboutInput = modal.querySelector(".popup__input_type_about");
 
-  let perfilName = document.querySelector(".profile__title");
-  let perfilAbout = document.querySelector(".profile__subtitle");
+  const perfilName = document.querySelector(".profile__title");
+  const perfilAbout = document.querySelector(".profile__subtitle");
 
   perfilName.textContent = nameInput.value;
   perfilAbout.textContent = aboutInput.value;
