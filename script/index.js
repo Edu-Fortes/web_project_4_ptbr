087a399f7af9b9d672c66c883811bd64092591bb
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-03-30 22:49:13 -0300

    hotfix(script): replace refact script on html
    Replace refacted script on HTML, rename it to index.js

--- script/index.js
+++ script/index.js
@@ -1,4 +1,62 @@
-//CARREGA OS 6 CARDS INICIAIS
+//Object with profile section query elements
+const profile = {
+  section: ".profile",
+  editBtn: ".button_edit",
+  inputName: ".popup__input_type_name",
+  inputAbout: ".popup__input_type_about",
+  perfilName: ".profile__title",
+  perfilAbout: ".profile__subtitle",
+};
+//Object with place section query elements
+const place = {
+  cardTemplate: "#card-template",
+  container: ".place",
+  addBtn: ".button_add",
+  card: ".place__card",
+  img: ".img",
+  photo: "img_card",
+  title: ".place__name",
+  inputTitle: ".popup__input_type_place",
+  inputImg: ".popup__input_type_img",
+  likeBtn: ".button__like",
+  likeBtnActive: "button__like_active",
+  trashBtn: ".button_trash",
+  trashBtnElement: "button_trash",
+  fig: ".place__fig",
+  likeBtnElement: "button__like",
+};
+//Object with popup section query elements
+const popup = {
+  popup: ".popup",
+  opened: "popup_opened",
+  formElement: ".popup__form",
+  profileForm: "popup__form_profile",
+  placeForm: "popup__form_place",
+  img: ".popup__img",
+  figCaption: ".popup__figcaption",
+};
+/* Constants to set each modal returned by NodeList.
+ * - One for profile modal - inputs to change profile info
+ * - One for place modal - inputs to add new card/post
+ * - One for photo modal - show bigger clicked card photo
+ * */
+const modals = document.querySelectorAll(popup.popup); //returns a NodeList with 3 popup results
+const profileModal = modals[0];
+const placeModal = modals[1];
+const photoModal = modals[2];
+//Global query selector consts
+const profileSection = document.querySelector(profile.section);
+const inputName = profileModal.querySelector(profile.inputName); //get name input field from the form inside modal to edit profile
+const inputAbout = profileModal.querySelector(profile.inputAbout); // get about field from form inside modal to edit profile
+const perfilName = profileSection.querySelector(profile.perfilName); //get element that contains text who displays pofile name on page
+const perfilAbout = profileSection.querySelector(profile.perfilAbout); //get element that contains text that display about info on page
+const formElement = document.querySelectorAll(popup.formElement);
+//Function to close all modals on page inicialization
+modals.forEach((modal) => {
+  modal.classList.remove(popup.opened);
+});
+
+//Loads 6 initial cards using "initialCards" const
 const initialCards = [
   {
     name: "Grand Canyon",
@@ -24,228 +82,203 @@ const initialCards = [
     name: "São Francisco",
     link: "./images/places_photo/san_francisco.jpg",
   },
-];
-
-const placeContainer = document.querySelector(".place");
-
+]; //Array with initial cards info to be loaded on page startup
+const placeContainer = document.querySelector(place.container);
+/* Get information from initialCard Array and construct a card with it.
+ * Loop trought each object inside the array*/
 initialCards.forEach((card) => {
-  const cardTemplate = document.querySelector("#card-template").content;
-  const cardElement = cardTemplate
-    .querySelector(".place__card")
-    .cloneNode(true);
-
-  const placeImg = cardElement.querySelector(".img"); //pega o src da imgem no card para alterar a imgem
-  const cardTitle = cardElement.querySelector(".place__name"); //pega o título do card para aterar
-
+  const cardTemplate = document.querySelector(place.cardTemplate).content;
+  const cardElements = cardTemplate.querySelector(place.card).cloneNode(true);
+  const placeImg = cardElements.querySelector(place.img);
+  const cardTitle = cardElements.querySelector(place.title);
   cardTitle.textContent = card.name;
   placeImg.src = card.link;
   placeImg.alt = `Imagem de capa da postagem ${card.name}`;
-
-  placeContainer.append(cardElement);
+  placeContainer.append(cardElements);
 });
-
-//***MODALS***/
-
-const modals = document.querySelectorAll(".popup");
-
-const profileModal = modals[0];
-const placeModal = modals[1];
-const photoModal = modals[2];
-
-//  FUNÇÃO PARA DEIXAR MODALS FECHADO NO CARREGAMENTO DA PÁGINA
-modals.forEach((item) => {
-  item.classList.remove("popup_opened");
-});
-
-//  PEGAR O BOTÃO DE ABRIR/FECHAR EDIÇÃO DE PERFIL
-const openProfileModalBtn = document.querySelector(".button_edit");
-const closeProfileModalBtn = profileModal.querySelector(".button_close");
-
-//  PEGAR BOTÃO DE ABRIR/FECHAR MODAL ADICIONAR FOTO
-const openPlaceModalBtn = document.querySelector(".button_add");
-const closePlaceModalBtn = placeModal.querySelector(".button_close");
-
-//PEGA CAMPOS DE INFORMAÇÃO DO MODAL EDITAR PERFIL
-const nameInput = profileModal.querySelector(".popup__input_type_name");
-const aboutInput = profileModal.querySelector(".popup__input_type_about");
-
-const perfilName = document.querySelector(".profile__title");
-const perfilAbout = document.querySelector(".profile__subtitle");
-
-//  FUNÇÃO PARA ABRIR/FECHAR OS MODALS
-const openCloseProfileModal = () => {
-  profileModal.classList.toggle("popup_opened");
-
-  nameInput.value = perfilName.textContent;
-  aboutInput.value = perfilAbout.textContent;
+//Function to open modals
+const openProfileModal = () => {
+  profileModal.classList.add(popup.opened);
+  inputName.value = perfilName.textContent;
+  inputAbout.value = perfilAbout.textContent;
 };
-
-const openClosePlaceModal = () => {
-  placeModal.classList.toggle("popup_opened");
+const openPlaceModal = () => {
+  placeModal.classList.add(popup.opened);
 };
-
-//  EVENT LISTENER NOS BOTÕES DE ABRIR E FECHAR
-openProfileModalBtn.addEventListener("click", openCloseProfileModal);
-closeProfileModalBtn.addEventListener("click", openCloseProfileModal);
-
-openPlaceModalBtn.addEventListener("click", openClosePlaceModal);
-closePlaceModalBtn.addEventListener("click", openClosePlaceModal);
-
-//  ENVIAR O FORMULARIO DO MODAL EDITAR PERFIL
-const formElement = profileModal.querySelector(".popup__form");
-
-function handleProfileFormSubmit(e) {
-  e.preventDefault();
-
-  perfilName.textContent = nameInput.value;
-  perfilAbout.textContent = aboutInput.value;
-
-  openCloseProfileModal();
-}
-
-formElement.addEventListener("submit", handleProfileFormSubmit);
-
-//  ENVIAR FORMULARIO PARA ADICIONAR NOVA FOTO
-
-const formPlaceElement = placeModal.querySelector(".popup__form");
-
-//ADICIONA O CARD DE FOTO
-function addPlaceCard(e) {
-  e.preventDefault();
-
-  //  PEGA TEMPLATE DO CARD
-  const cardTemplate = document.querySelector("#card-template").content;
-  const cardElement = cardTemplate
-    .querySelector(".place__card")
-    .cloneNode(true);
-
-  const placeImg = cardElement.querySelector(".img"); //pega o src da imgem no card para alterar a imgem
-  const cardTitle = cardElement.querySelector(".place__name"); //pega o título do card para aterar
-  const placeNameInput = placeModal.querySelector(".popup__input_type_place"); //pega o campo de inserção do titulo do modal places
-  const imgInput = placeModal.querySelector(".popup__input_type_img"); //pega o campo de input da url da imagem
-
-  cardTitle.textContent = placeNameInput.value;
-  placeImg.src = imgInput.value;
-  placeImg.alt = `Imagem de capa da postagem ${cardTitle.textContent}`;
-
-  openClosePlaceModal();
-
-  placeContainer.prepend(cardElement);
-
-  placeNameInput.value = "";
-  imgInput.value = "";
-
-  const likeBtn = cardElement.querySelector(".button__like");
-
-  likeBtn.classList.remove("button__like_active");
-
-  likeBtn.addEventListener("click", (e) => {
-    e.target.classList.toggle("button__like_active");
-  });
-
-  applyCardOverlay();
-  onCLickOpenPhoto();
-  removeCard();
-}
-
-formPlaceElement.addEventListener("submit", addPlaceCard);
-
-//REMOVE O ESTADO ATIVO DO BOTÃO LIKE NO CARREGAMENTO DA PÁGINA
-const likeButton = document.querySelectorAll(".button__like");
-
-likeButton.forEach((button) => {
-  button.classList.remove("button__like_active");
-});
-
-//  BOTÃO CURTIR ATIVO
-
-likeButton.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    e.target.classList.toggle("button__like_active");
-  });
-});
-
-//  ZOOM NA FOTO QUANDO CLICAR
-//PEGAR "BOTÃO" PARA ABRIR/FECHAR ZOOM NA FOTO
-let childPlaceNodes = placeContainer.childNodes;
-
-const openPhoto = placeContainer.querySelectorAll(".img_card");
-const closePhotoBtn = photoModal.querySelector(".button_close");
-
-//FUNÇÃO PARA ABRIR O MODAL DAS FOTOS
-const openPhotoModal = () => {
-  changeFigCaption();
-  changePhotoURL();
-  photoModal.classList.add("popup_opened");
+//Functions to close modals
+const closeProfileModal = () => {
+  profileModal.classList.remove(popup.opened);
 };
-
-//FUNÇÃO PARA FECHAR O MODAL DA FOTO
+const closePlaceModal = () => {
+  placeModal.classList.remove(popup.opened);
+};
 const closePhotoModal = () => {
-  photoModal.classList.remove("popup_opened");
+  photoModal.classList.remove(popup.opened);
 };
 
-//QUANDO CLICAR NO X FECHA O MODAL DA FOTO
-closePhotoBtn.addEventListener("click", closePhotoModal);
-
-//FUNÇÃO PARA PEGAR A URL DA IMAGEM NO GRID E ABRIR A FOTO
+/* Event listener for click inside profile section, function will ckeck
+ * witch element was clicked and perform an action*/
+profileSection.addEventListener("click", handleProfileSectionClick);
+function handleProfileSectionClick(event) {
+  const target = event.target;
+  //Check if the clicked element is the edit profile button
+  if (target.classList.contains("button_edit")) {
+    openProfileModal(target);
+    return;
+  }
+  //Check if the clicked element is the edit profile button image (pen icon)
+  if (target.classList.contains("img_button_edit")) {
+    openProfileModal(target);
+    return;
+  }
+  //Check if the clicked element is the edit profile button
+  if (target.classList.contains("button_add")) {
+    openPlaceModal(target);
+    return;
+  }
+  //Check if the clicked element is the edit profile button image (plus sign)
+  if (target.classList.contains("img_button_add")) {
+    openPlaceModal(target);
+    return;
+  }
+}
+/* Event listener for clicks on close button, function will ckeck
+ * witch modal close button was clicked and perfom an action*/
+modals.forEach((modal) => {
+  modal.addEventListener("click", handleModalsClick);
+});
+function handleModalsClick(event) {
+  const target = event.target;
+  if (target.classList.contains("img_button_close")) {
+    closeProfileModal(target);
+    closePlaceModal(target);
+    closePhotoModal(target);
+  }
+}
+//Listener to catch form submition from edit profile and add photo
+formElement.forEach((form) => {
+  form.addEventListener("submit", handleFormSubmit);
+});
+//Function to watch witch form was submited
+function handleFormSubmit(event) {
+  const target = event.target;
+  //Check if the clicked element is the Save button in Profile Modal
+  if (target.classList.contains(popup.profileForm)) {
+    event.preventDefault();
+    formSubmitProfile(target);
+    return;
+  }
+  //Check if clicked element is the Add button in Add Place Modal
+  if (target.classList.contains(popup.placeForm)) {
+    event.preventDefault(); //prevents re-load of page on form submition
+    formSubmitAddPlace(target);
+    return;
+  }
+}
+//Function to change input fieds from profile setion
+function formSubmitProfile() {
+  perfilName.textContent = inputName.value; //change page user name by user input
+  perfilAbout.textContent = inputAbout.value; //change page about info by user input
+  closeProfileModal();
+}
+//Function to enter values to add new card to beginning of place container
+function formSubmitAddPlace() {
+  const cardTemplate = document.querySelector(place.cardTemplate).content;
+  const cardElement = cardTemplate.querySelector(place.card).cloneNode(true);
+  const placeImg = cardElement.querySelector(place.img);
+  const cardTitle = cardElement.querySelector(place.title);
+  const inputCardTitle = placeModal.querySelector(place.inputTitle);
+  const inputCardImg = placeModal.querySelector(place.inputImg);
+  const likeBtn = cardElement.querySelector(place.likeBtn);
+  cardTitle.textContent = inputCardTitle.value; //card name entered by user
+  placeImg.src = inputCardImg.value; //URL to image
+  placeImg.alt = `Imagem de capa da postagem ${cardTitle.textContent}`;
+  closePlaceModal(); //close modal when submit form due preventDefault function
+  placeContainer.prepend(cardElement); //add card to beginning of grid place
+  inputCardTitle.value = ""; //reset input fields after form submition
+  inputCardImg.value = ""; //reset input field after form submition
+  likeBtn.classList.remove(place.likeBtnActive); //remove initial active state of like button
+  handleCardEvent("click", removeCard);
+  handleCardEvent("mouseenter", applyCardOpacity);
+  handleCardEvent("mouseleave", removeCardOpacity);
+}
+//Remove active state of like buttons
+const likeBtn = document.querySelectorAll(place.likeBtn);
+const likeBtnArray = Array.from(likeBtn); //create an Array from NodeList returned by querySelectorAll
+likeBtnArray.forEach((button) => {
+  button.classList.remove(place.likeBtnActive);
+});
+//Handler function to watch witch button was clicked inside place container
+placeContainer.addEventListener("click", handlePlaceContainerClick);
+function handlePlaceContainerClick(event) {
+  const target = event.target;
+  //Check if clicked element is a like button
+  if (target.classList.contains(place.likeBtnElement)) {
+    handleLikeButtonClick(target);
+    return;
+  }
+  //Ckeck if clicked element is a photo
+  if (target.classList.contains(place.photo)) {
+    handleOpenPhoto(target);
+    return;
+  }
+}
+//Cosolidated forEach loop for chieldNotes in one function that accepts callback
+const childPlaceNodes = placeContainer.childNodes;
+function forEachCard(callback) {
+  childPlaceNodes.forEach(callback);
+}
+//Funtion to toggle state of like button (active to deactive and vice versa)
+function handleLikeButtonClick(target) {
+  target.classList.toggle(place.likeBtnActive);
+}
+//Function to open photo modal (zoom) when clicking on a photo
 let photoSrc;
 let figCaption;
-
-function onCLickOpenPhoto() {
-  childPlaceNodes.forEach((card) => {
-    const img = card.querySelector(".img_card");
-    const cardTitle = card.querySelector(".place__name").textContent;
-    img.addEventListener("click", (e) => {
-      photoSrc = e.target.src;
-      figCaption = cardTitle;
-      openPhotoModal();
-    });
-  });
-}
-
-//QUANDO CLICAR NA FOTO ABRIR O MODAL COM A FOTO CLICADA
-onCLickOpenPhoto();
-
-//MUDA URL DA IMAGEM NO MODAL DE FOTO
-const changePhotoURL = () => {
-  const zoomedPhoto = photoModal.querySelector(".popup__img");
+function handleOpenPhoto(target) {
+  const figAlt = target.alt; //get alt atribute from image to set figcaption when photo is zoomed
+  const zoomedPhoto = photoModal.querySelector(popup.img);
+  const photoCaption = photoModal.querySelector(popup.figCaption);
+  photoSrc = target.src;
+  figCaption = figAlt.slice(27); //slice string lenght to use only the title of the post
+  photoCaption.textContent = figCaption; //add figcaption when image opens
   zoomedPhoto.src = photoSrc;
   zoomedPhoto.alt = `Imagem ampliada da postagem ${figCaption}`;
-};
-
-//MUDAR A FIGCAPTION QUANDO ABRIR A IMAGEM
-const changeFigCaption = () => {
-  const photoCaption = photoModal.querySelector(".popup__figcaption");
-  photoCaption.textContent = figCaption;
-};
-
-//OPACITY DA IMG DO CARD QUANDO HOVER NO TRASH
-function applyCardOverlay() {
-  childPlaceNodes.forEach((card) => {
-    const overlay = card.querySelector(".fig");
-    const trashBtn = card.querySelector(".button_trash");
-    const opacityValue = 0.5;
-
-    trashBtn.addEventListener("mouseenter", (e) => {
-      overlay.style.setProperty("opacity", opacityValue);
-    });
-
-    trashBtn.addEventListener("mouseleave", (e) => {
-      overlay.style.removeProperty("opacity");
-    });
+  photoModal.classList.add(popup.opened); //open photo modal
+}
+//Function to handle Events happening inside each card, need a callback function
+function handleCardEvent(eventType, callback) {
+  forEachCard((card) => {
+    const trashBtn = card.querySelector(place.trashBtn);
+    trashBtn.addEventListener(eventType, callback);
   });
 }
-
-applyCardOverlay();
-
-//  EXCLUIR POSTAGEM
-function removeCard() {
-  childPlaceNodes.forEach((card) => {
-    const trashBtn = card.querySelector(".button_trash");
-    trashBtn.addEventListener("click", (e) => {
-      card.remove();
-    });
-  });
+//Callback function to remove a card
+function removeCard(event) {
+  const target = event.target;
+  target.closest(place.card).remove();
+}
+//Callback function to apply opacity effect when hovering over trash button
+function applyCardOpacity(event) {
+  const target = event.target;
+  const cardElement = target.parentElement.parentElement;
+  const overlay = cardElement.querySelector(place.fig);
+  const opacityValue = 0.5;
+  overlay.style.setProperty("opacity", opacityValue);
 }
 
-removeCard();
+//Callback function to remove opacity from imag card
+function removeCardOpacity(event) {
+  const target = event.target;
+  const cardElement = target.parentElement.parentElement;
+  const overlay = cardElement.querySelector(place.fig);
+  const opacityValue = 0.5;
+  overlay.style.removeProperty("opacity");
+}
+//Remove card on trash button click
+handleCardEvent("click", removeCard);
+//Apply opacity on card when hovering trash button
+handleCardEvent("mouseenter", applyCardOpacity);
+//Remove opacity when hovering trash button. Opacity is been applied by CSS
+handleCardEvent("mouseleave", removeCardOpacity);
+//REMOVER CLASSE BLOCO fig, VERIFICAR SE AINDA USA ESSA CLASSE EM ALGUM ELEMENTO.
