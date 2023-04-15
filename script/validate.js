d6b8ca6c9a5959a7051fa74ee4fbbaeada483249
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-04-14 22:35:38 -0300

    feat(script): add functions to control validation

---
+++ script/validate.js
@@ -0,0 +1,56 @@
+const formsConfig = {
+  formSelector: ".popup__form",
+  inputSelector: ".popup__input",
+  submitButtonSelector: ".button",
+  inactiveButtonClass: "button_disabled",
+  inputErrorClass: "popup__input_type_error",
+  errorClass: "popup__error_visible",
+};
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(formsConfig.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(formsConfig.errorClass);
+};
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}--error`);
+  inputElement.classList.remove(formsConfig.inputErrorClass);
+  errorElement.classList.remove(formsConfig.errorClass);
+  errorElement.textContent = "";
+};
+
+const isValid = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(
+    formElement.querySelectorAll(formsConfig.inputSelector)
+  );
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener("input", () => {
+      isValid(formElement, inputElement);
+    });
+  });
+};
+
+const enableValidation = () => {
+  const formList = Array.from(
+    document.querySelectorAll(formsConfig.formSelector)
+  );
+
+  formList.forEach((formElement) => {
+    setEventListeners(formElement);
+  });
+};
+
+enableValidation();
+
+//veririficar inputElement.validationMessage (Acho que é um objeto com a messagem de erro)
