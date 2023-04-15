05ad30fb7482be66d8be0a0b67d49234c1b3d08e
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-04-15 10:57:21 -0300

    feat(script): add feature to disable buttons

--- script/validate.js
+++ script/validate.js
@@ -1,10 +1,11 @@
 const formsConfig = {
   formSelector: ".popup__form",
   inputSelector: ".popup__input",
-  submitButtonSelector: ".button",
+  submitButtonSelector: `.button[type="submit"]`,
   inactiveButtonClass: "button_disabled",
   inputErrorClass: "popup__input_type_error",
   errorClass: "popup__error_visible",
+  disableClass: "button_disabled",
 };
 
 const showInputError = (formElement, inputElement, errorMessage) => {
@@ -29,14 +30,35 @@ const isValid = (formElement, inputElement) => {
   }
 };
 
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(formsConfig.disableClass);
+  } else {
+    buttonElement.classList.remove(formsConfig.disableClass);
+  }
+};
+
 const setEventListeners = (formElement) => {
   const inputList = Array.from(
     formElement.querySelectorAll(formsConfig.inputSelector)
   );
+  const buttonElement = formElement.querySelector(
+    formsConfig.submitButtonSelector
+  );
+
+  console.log(buttonElement);
+  toggleButtonState(inputList, buttonElement);
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener("input", () => {
       isValid(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 };
