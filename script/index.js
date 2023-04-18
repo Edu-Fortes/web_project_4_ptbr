931c8ce9bef3ee74fc6151dabbbabd4274f0a434
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-04-18 00:14:31 -0300

    hotfix(images): remove places images
    Remove images of places from images folder to decrease project size

--- script/index.js
+++ script/index.js
@@ -60,27 +60,27 @@ modals.forEach((modal) => {
 const initialCards = [
   {
     name: "Grand Canyon",
-    link: "./images/places_photo/grand_canion.jpg",
+    link: "https://images.unsplash.com/photo-1615551043360-33de8b5f410c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=876&q=80",
   },
   {
     name: "Represa Hoover",
-    link: "./images/places_photo/hover_dam.jpg",
+    link: "https://images.unsplash.com/photo-1546047090-bfd187e30781?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
   },
   {
     name: "Nova Iorque",
-    link: "./images/places_photo/new_york.jpg",
+    link: "https://images.unsplash.com/photo-1555109307-f7d9da25c244?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=873&q=80",
   },
   {
     name: "California",
-    link: "./images/places_photo/california_pier.jpg",
+    link: "https://images.unsplash.com/photo-1562748544-21a0bd1a9a6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   },
   {
     name: "Las Vegas",
-    link: "./images/places_photo/las_vegas.jpg",
+    link: "https://images.unsplash.com/photo-1577334928618-2ff2bf09e827?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80",
   },
   {
     name: "São Francisco",
-    link: "./images/places_photo/san_francisco.jpg",
+    link: "https://images.unsplash.com/photo-1521747116042-5a810fda9664?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
   },
 ]; //Array with initial cards info to be loaded on page startup
 const placeContainer = document.querySelector(place.container);
