b7f833ea91be297712f41210bf043fa983e63772
Edu-Fortes <eduardofortes.ma@gmail.com>
2023-05-02 00:31:05 -0300

    refact(script): add function to close photo modal
    Add function in utils.js file to close photo modal cliking on close button or outside of modal window.

--- script/script.js
+++ script/script.js
@@ -26,21 +26,6 @@ const initialCards = [
   },
 ];
 
-/* Constants to set each modal returned by NodeList.
- * - One for profile modal - inputs to change profile info
- * - One for place modal - inputs to add new card/post
- * - One for photo modal - show bigger clicked card photo
- * */
-const modals = document.querySelectorAll(".popup"); //returns a NodeList with 3 popup results
-const profileModal = modals[0];
-const placeModal = modals[1];
-const photoModal = modals[2];
-
-//Close all modals on page loading
-modals.forEach((modal) => {
-  modal.classList.remove("popup_opened");
-});
-
 //Create initial 6 cards from initialCards object
 initialCards.forEach((object) => {
   const card = new Card(object, "#card-template");
--- script/utils.js
+++ script/utils.js
@@ -1,24 +1,27 @@
-//Function to open modals
-const openProfileModal = () => {
-  profileModal.classList.add(popup.opened);
-  inputName.value = perfilName.textContent;
-  inputAbout.value = perfilAbout.textContent;
-  addKeydownListener();
-};
-const openPlaceModal = () => {
-  placeModal.classList.add(popup.opened);
-  addKeydownListener();
-};
-//Functions to close modals
-const closeProfileModal = () => {
-  profileModal.classList.remove(popup.opened);
-  removeKeydownListener();
-};
-const closePlaceModal = () => {
-  placeModal.classList.remove(popup.opened);
-  removeKeydownListener();
-};
-const closePhotoModal = () => {
-  photoModal.classList.remove(popup.opened);
-  removeKeydownListener();
-};
+/* Constants to set each modal returned by NodeList.
+ * - One for profile modal - inputs to change profile info
+ * - One for place modal - inputs to add new card/post
+ * - One for photo modal - show bigger clicked card photo
+ * */
+const modals = document.querySelectorAll(".popup"); //returns a NodeList with 3 popup results
+const profileModal = modals[0];
+const placeModal = modals[1];
+const photoModal = modals[2];
+
+//Close all modals on page loading
+modals.forEach((modal) => {
+  modal.classList.remove("popup_opened");
+});
+
+function handleModalsClick(event) {
+  if (
+    event.target.classList.contains("img_button_close") ||
+    event.target.classList.contains("popup")
+  ) {
+    event.target.closest(".popup").classList.remove("popup_opened");
+  }
+}
+
+modals.forEach((modal) => {
+  modal.addEventListener("click", handleModalsClick);
+});
